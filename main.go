4f014313f2a5a7baad54933fccfec980555be5ce
kaznishi <kaz.nishimu@gmail.com>
2018-08-12 12:49:02 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-http-utils/logger"
+	"os"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		res.Write([]byte("Hello, World!!"))
+	})
+
+	http.ListenAndServe(":8080", logger.Handler(mux, os.Stdout, logger.DevLoggerType))
+}

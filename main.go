eb0a3a072047f66a7690de1bf59d90376c10805b
kaznishi <kaz.nishimu@gmail.com>
2018-08-15 08:41:20 +0900

    .

--- main.go
+++ main.go
@@ -1,17 +1,59 @@
 package main
 
 import (
-	"net/http"
+	"github.com/spf13/viper"
+	"fmt"
+	"net/url"
+	"database/sql"
+	"log"
 	"os"
+	"time"
 
-	"github.com/go-http-utils/logger"
+	"github.com/labstack/echo"
+	"github.com/kaznishi/clean-arch-golang/infra/dao"
+	"github.com/kaznishi/clean-arch-golang/usecase"
+	"github.com/kaznishi/clean-arch-golang/adapter/handler"
 )
 
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		res.Write([]byte("Hello, World!!"))
-	})
+func init() {
+	viper.SetConfigFile(`config.yaml`)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	if viper.GetBool(`debug`) {
+		fmt.Println("Service RUN on DEBUG mode")
+	}
+}
 
-	http.ListenAndServe(":8080", logger.Handler(mux, os.Stdout, logger.DevLoggerType))
+func main() {
+	dbHost := viper.GetString(`database.host`)
+	dbPort := viper.GetString(`database.port`)
+	dbUser := viper.GetString(`database.user`)
+	dbPass := viper.GetString(`database.pass`)
+	dbName := viper.GetString(`database.name`)
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+	val := url.Values{}
+	val.Add("parseTime", "1")
+	val.Add("loc", "Asia/Tokyo")
+	dsn := fmt.Sprintf("%s?%s", connection, val.Encode())
+	dbConn, err := sql.Open(`mysql`, dsn)
+	if err != nil && viper.GetBool("debug") {
+		fmt.Println(err)
+	}
+	err = dbConn.Ping()
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	defer dbConn.Close()
+	e := echo.New()
+	authorRepository := dao.NewAuthorDAO(dbConn)
+	articleRepository := dao.NewArticleDAO(dbConn)
+	timeoutContext := time.Duration(viper.GetInt("context.timetout")) * time.Second
+	articleUsecase := usecase.NewArticleUsecase(articleRepository, authorRepository, timeoutContext)
+	handler.NewArticleHandler(e, articleUsecase)
+
+	e.Start(viper.GetString("server.address"))
 }
